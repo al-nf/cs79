07f58a0558779831d1a097f53b71124f465451fa
al-nf <alankjfung@gmail.com>
2025-09-29 19:41:13 -0700

    oop shenanigans

--- lab2afung/shuffle.cpp
+++ lab2afung/shuffle.cpp
@@ -11,17 +11,17 @@
 namespace lab2afung {
 	// implement Fisher-Yates here
 	void Deck::shuffle(void) {
-        Deck::next = 0;
+        next = 0;
         for (int i = Deck::CARDS_PER_DECK-1; i >= 1; --i) {
             int j = std::rand()%(Deck::CARDS_PER_DECK-i) + i;
-            std::swap(Deck::cards[i], Deck::cards[j]);
+            std::swap(cards[i], cards[j]);
         }
     }
 
 	// deal out one card
 	const Card &Deck::deal() {
-        if (Deck::next >= Deck::CARDS_PER_DECK - Deck::guard) {
-            Deck::shuffle();
+        if (next >= Deck::CARDS_PER_DECK - guard) {
+            shuffle();
         }
 		return cards[Deck::next++];
 	}
